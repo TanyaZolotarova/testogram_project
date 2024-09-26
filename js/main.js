0d8fddca976f982e92286aa93528da1f516927d5
TanyaZolotarova <51660611+TanyaZolotarova@users.noreply.github.com>
2024-09-26 18:46:18 +0300

    added new module full photo view and data-id on mini photo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 import {generatePhotosArray} from './photoGenerator.js';
 import {renderPhoto} from './photoRender.js'
+import {showBigPicture} from './fullPhotoView.js';
 
 const photosArray = generatePhotosArray(25);
 renderPhoto(photosArray);
+
+const picturesContainer = document.querySelector('.pictures');
+picturesContainer.addEventListener('click', (evt) => {
+  const pictureEl = evt.target.closest('.picture');
+  if (pictureEl) {
+    const pictureId = pictureEl.dataset.id;
+    const selectedPhoto = photosArray.find(photo => photo.id === Number(pictureId));
+    showBigPicture(selectedPhoto);
+  }
+});
